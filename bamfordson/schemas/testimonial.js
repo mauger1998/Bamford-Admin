87ab148b82bd1ca03e2d5aa0b62faa8e87bbe859
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-01 18:34:54 +0000

    Create and deploy testimonial cms

---
+++ bamfordson/schemas/testimonial.js
@@ -0,0 +1,22 @@
+export default {
+    name:"testimonial",
+    type: "document",
+    title: "Testimonials",
+    fields: [
+        {
+            name: "rating",
+            type: "number",
+            title: "Rating"
+        },
+        {
+            name: "comment",
+            type: "text",
+            title: "Comment",
+        },
+        {
+            name: "name",
+            type: "string",
+            title: "Name",
+        },
+    ]
+}
